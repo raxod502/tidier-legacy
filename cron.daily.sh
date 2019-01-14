173615edfa37be9ead94e58058db470582d34010
Radon Rosborough <radon.neon@gmail.com>
2019-01-14 13:02:55 -0700

    Process more repositories

--- cron.daily.sh
+++ cron.daily.sh
@@ -1,3 +1,3 @@
-for repo in radian straight.el el-patch; do
+for repo in radian straight.el el-patch prescient.el blackout diary-manager; do
         java -jar no-carrier.jar "raxod502/$repo" "waiting on response" 90
 done
